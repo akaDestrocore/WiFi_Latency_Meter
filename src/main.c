a13a935fc0134ca8dd175600c3075fbf0791e3e1
akaDestrocore <blackxcat14@yahoo.com>
2025-05-25 04:07:09 +0300

    Create `include/lwipopts.h`

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 
-
+/**
+  * @brief  The application entry point.
+  * @return int
+  */
 int main()
 {
+    // Initialize standard I/O
     stdio_init_all();
+    // delay to allow the serial port to initialize
+    sleep_ms(2000);
+    printf("Wi-Fi Latency Meter Starting...\r\n");
+
 
     while (true) {
         printf("Hello, world!\n");
