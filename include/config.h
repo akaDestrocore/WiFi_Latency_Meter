820c9374b98bf1a43da531aaf7e9e58385d1b936
akaDestrocore <blackxcat14@yahoo.com>
2025-05-28 01:46:44 +0300

    Update `include/config.h`

--- include/config.h
+++ include/config.h
@@ -2,21 +2,24 @@
 #define CONFIG_H
 
 // Network configuration
-#define ROUTER_IP_ADDR      "192.168.2.1"
-#define WIFI_SSID           "SomeSSID"
-#define WIFI_PASSWORD       "SomePassword"
+#define ROUTER_IP_ADDR          "192.168.2.1"
+#define WIFI_SSID               "SomeSSID"
+#define WIFI_PASSWORD           "SomePassword"
 
 // Measurememnt configuration
-#define PING_TIMEOUT_MS     2000
-#define MAX_PING_COUNT       5
+#define PING_TIMEOUT_MS         2000
+#define MAX_PING_COUNT          5
+#define MAX_RETRY_COUNT         5
+#define INITIAL_RETRY_DELAY_MS  1000
+#define MEASUREMENT_INTERVAL_MS 5000
 
 // InfluxDB configuration
 #define INFLUX_RETRY_DELAY_MS   1000
-#define INFLUXDB_IP     "SomeIP"
-#define INFLUXDB_PORT   8086
-#define INFLUXDB_ORG    "Wi-Fi%20Latency"
-#define INFLUXDB_BUCKET "Data"
-#define INFLUXDB_TOKEN  "FjH5x9Z1qTgmxPChyd2Av8zVTrDsfI-BAfmadWYOViST-1BW40koPQoXupU5oNAmE8-4CIDNBBf8kWskdNRo7Q=="
+#define INFLUXDB_IP             "SomeIP"
+#define INFLUXDB_PORT           8086
+#define INFLUXDB_ORG            "Wi-Fi%20Latency"
+#define INFLUXDB_BUCKET         "Data"
+#define INFLUXDB_TOKEN          "FjH5x9Z1qTgmxPChyd2Av8zVTrDsfI-BAfmadWYOViST-1BW40koPQoXupU5oNAmE8-4CIDNBBf8kWskdNRo7Q=="
 
 #define DEBUG 1
 #ifdef DEBUG
