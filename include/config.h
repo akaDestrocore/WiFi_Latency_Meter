f770b57158850d2d6a351cc2479307ac843512b2
akaDestrocore <blackxcat14@yahoo.com>
2025-05-27 12:08:03 +0300

    Update `include/config.h` and `src/main.c`

--- include/config.h
+++ include/config.h
@@ -1,8 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// Network configuration
+#define ROUTER_IP_ADDR    "192.168.2.1"
+
 // Measurememnt configuration
 #define PING_TIMEOUT_MS     2000
+#define MAX_PING_COUNT       5
 
 #define DEBUG 1
 #ifdef DEBUG
--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "sensors.h"
+#include "ping.h"
 
 
 /**
@@ -19,13 +20,26 @@ int main()
 
     // Initialize ADC to measure temperature
     temperature_init();
-
-    /* Read temperature */
-    float temperature = temperature_read_celsius();
-    printf("Temperature: %.2f°C", temperature);
-  
+	
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+
+		Ping_Handle_t ping_handle;
+		bool ping_success = ping_measure(&ping_handle, ROUTER_IP_ADDR);
+
+		/* Read temperature */
+		float temperature = temperature_read_celsius();
+		printf("Temperature: %.2f°C\r\n", temperature);
+
+		if(true == ping_success && ping_handle.received > 0) {
+			uint64_t avg_rtt_us, min_rtt_us, max_rtt_us, jitter_us;
+			uint8_t loss_p;
+
+			ping_calculate_stats(&ping_handle, &avg_rtt_us, &min_rtt_us, &max_rtt_us, &jitter_us, &loss_p);
+
+			DBG("Packets: sent=%u, received=%u, loss=%u%%\r\n", ping_handle.sent, ping_handle.received, loss_p);
+			DBG("Avg RTT: %llu us, Min RTT: %llu us, Max RTT: %llu us, Jitter: %llu us\r\n", avg_rtt_us, min_rtt_us, max_rtt_us, jitter_us);
+		}
+
+		sleep_ms(2000); 
     }
 }
